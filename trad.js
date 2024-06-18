9496cc8c4c6f89c56abdf46ad4b80734a67e9d70
TheProjectWebSite <gamechanging0123@gmail.com>
2024-06-18 10:48:14 +0800

    Add files via upload

---
+++ trad.js
@@ -0,0 +1,30 @@
+document.getElementById('menu-icon').addEventListener('click', () => {
+    const navbar = document.querySelector('.navbar');
+    navbar.style.display = (navbar.style.display === 'flex') ? 'none' : 'flex';
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const next = document.querySelector('.next');
+    const prev = document.querySelector('.prev');
+
+    function nextSlide() {
+        let items = document.querySelectorAll('.item');
+        document.querySelector('.slide').appendChild(items[0]);
+    }
+
+    function prevSlide() {
+        let items = document.querySelectorAll('.item');
+        document.querySelector('.slide').prepend(items[items.length - 1]);
+    }
+
+    next.addEventListener('click', nextSlide);
+
+    prev.addEventListener('click', prevSlide);
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'ArrowLeft') {
+            prevSlide(); 
+        } else if (event.key === 'ArrowRight') {
+            nextSlide(); 
+        }
+    });
+});
